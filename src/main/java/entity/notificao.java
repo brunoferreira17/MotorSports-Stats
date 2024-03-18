d773aff23375ce39a1e18df89b47564f1e98561f
DiogoPereira04 <diogopereir99@gmail.com>
2024-03-18 16:39:13 +0000

    pega la

--- src/main/java/entity/notificao.java
+++ src/main/java/entity/notificao.java
@@ -1,2 +1,18 @@
-package entity;public class notificao {
+package entity;
+
+import javax.persistence.*;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "\"notificacao\"")
+public class notificao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "\"id_notificacao\"", nullable = false)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 50)
+    private String nome;
 }
