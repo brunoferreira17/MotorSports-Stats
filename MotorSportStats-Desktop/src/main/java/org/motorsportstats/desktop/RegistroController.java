dda90f68177c033894b23500c322a34bb46c5565
brunoferreira17 <brunoferreiraferreira17@gmail.com>
2024-04-09 17:34:58 +0100

    1893

--- MotorSportStats-Desktop/src/main/java/org/motorsportstats/desktop/RegistroController.java
+++ MotorSportStats-Desktop/src/main/java/org/motorsportstats/desktop/RegistroController.java
@@ -2,15 +2,35 @@ package org.motorsportstats.desktop;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class RegistroController
 {
     @FXML
     private Button ButCancelarRegistro;
+    @FXML
+    private TextField nome_usuario;
+    @FXML
+    private TextField password_usuario;
+
 
     @FXML
     private void handleButCancelarRegistro()
     {
         Recursos.SceneSwitcher.switchScene("paginainicial.fxml",ButCancelarRegistro);
     }
+
+    @FXML
+    public String getnome_usuario()
+    {
+        return nome_usuario.getText();
+    }
+
+    @FXML
+    public String getpassword_usuario()
+    {
+        return password_usuario.getText();
+    }
+
+
 }
