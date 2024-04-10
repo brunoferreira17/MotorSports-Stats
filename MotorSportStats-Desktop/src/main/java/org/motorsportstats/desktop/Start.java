fa0f18f956bd7eabfafbde836222f2f1bd058800
brunoferreira17 <brunoferreiraferreira17@gmail.com>
2024-04-10 22:28:57 +0100

    1893

--- MotorSportStats-Desktop/src/main/java/org/motorsportstats/desktop/Start.java
+++ MotorSportStats-Desktop/src/main/java/org/motorsportstats/desktop/Start.java
@@ -21,7 +21,8 @@ public class Start extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
     }
 }
